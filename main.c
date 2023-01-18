6cb3c447632c892c5bbfbaadd809dc19d0420373
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-01-18 18:02:16 +0100

    cpu_usage on stat

--- main.c
+++ main.c
@@ -20,7 +20,6 @@ DIR *dp;
             if(!isdigit(ep->d_name[0]))
                 continue;
 
-
             char* stat_proc = NULL;
             stat_proc = malloc(sizeof(char)*255);
             strcat(stat_proc, "/proc/");
@@ -32,7 +31,37 @@ DIR *dp;
             char* stat_str = NULL;
             stat_str = malloc(sizeof(char)*255);
             fgets(stat_str, 100, stat_ptr);
-            printf("%s\n", stat_str);
+
+            char* stat_value; 
+            int stat_index = 0;
+
+            stat_value = strtok(stat_str, " ");
+
+            float stime_cpu_value;
+            float ultime_cpu_value;
+            float starttime_cpu_value;
+
+            while(stat_value != NULL) {
+
+
+                if (stat_index == 0)
+                    printf("PID: %s\n", stat_value);
+                else if (stat_index == 1)
+                    printf("PID: %s\n", stat_value);
+                else if (stat_index == 13)  
+                    stime_cpu_value = atoi(stat_value);
+                else if (stat_index == 14)
+                    ultime_cpu_value = atoi(stat_value);
+                else if (stat_index == 22)
+                    starttime_cpu_value = atoi(stat_value);
+
+                stat_index++;            
+                stat_value = strtok(NULL, " ");
+            }
+
+            float cpu_usage = (stime_cpu_value + ultime_cpu_value)/starttime_cpu_value;
+            printf("CPU USAGE: %.6f\n", cpu_usage);
+
 
             stat_proc = NULL;
 
