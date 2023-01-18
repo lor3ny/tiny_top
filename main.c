4a81ec0c69abe9d809a749c2d86cccc355101f47
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-01-18 16:57:46 +0100

    proc access, with stat file

--- main.c
+++ main.c
@@ -2,20 +2,51 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(){
-    DIR *dp;
+
+
+int process_monitor(){
+DIR *dp;
     struct dirent *ep;     
     dp = opendir ("/proc");
+    
     if (dp != NULL) {
-    while ((ep = readdir (dp)) != NULL)
-        puts (ep->d_name);
-          
+        while ((ep = readdir (dp)) != NULL) {
+
+            if(!isdigit(ep->d_name[0]))
+                continue;
+
+
+            char* stat_proc = NULL;
+            stat_proc = malloc(sizeof(char)*255);
+            strcat(stat_proc, "/proc/");
+            strcat(stat_proc, ep->d_name);
+            strcat(stat_proc,"/stat");
+
+            FILE *stat_ptr = fopen(stat_proc, "r");
+            
+            char* stat_str = NULL;
+            stat_str = malloc(sizeof(char)*255);
+            fgets(stat_str, 100, stat_ptr);
+            printf("%s\n", stat_str);
+
+            stat_proc = NULL;
+
+            
+        }             
         (void) closedir (dp);
         return 0;
+
     } else {
         perror ("Couldn't open the directory");
         return -1;
     }
+}
+
+int main(){
+    process_monitor();
 }
